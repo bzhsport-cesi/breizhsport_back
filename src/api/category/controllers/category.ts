299e7df77f1735823c514361bff60b477c769154
Simon PROVOST <sprovost@secom.fr>
2025-02-06 15:57:09 +0100

    Add new API route for retrieving category tree data and consolidate product data retrieval

--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -4,4 +4,56 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::category.category');
+export default factories.createCoreController('api::category.category', ({ strapi }) => ({
+    async getTreeData(ctx) {
+        //TODO parse returned data to IStrapiAPIResponse
+        try {
+            const { slug } = ctx.params;
+            const currentPath = slug
+
+            const currentCategory = await strapi.query("api::category.category").findOne({
+                where: {
+                    slug: `/${currentPath}`
+                }
+            })
+
+            if (!currentCategory) return ctx.notFound('Category not found');
+
+
+            const children = await strapi.documents("api::category.category").findMany({
+                filters: {
+                    slug: { $startsWith: `/${currentPath}/`, $notEq: `/${currentPath}` },
+                    depth: currentCategory.depth + 1
+                }
+            })
+
+            const products = await strapi.documents("api::product.product").findMany({
+                filters: {
+                    category: {
+                        slug: { $startsWith: `/${currentPath}` }
+                    }
+                },
+                populate: {
+                    category: true,
+                    defaultVariant: {
+                        populate: '*'
+                    },
+                    variants: {
+                        populate: '*'
+                    }
+                }
+            })
+
+
+            return {
+                currentCategory,
+                children,
+                products
+            };
+
+        } catch (err) {
+            ctx.throw(500, err.message);
+        }
+    }
+}))
+
