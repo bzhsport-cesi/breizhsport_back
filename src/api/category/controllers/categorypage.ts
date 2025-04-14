7ceb41d99d7df0e3893dd59bb11c1f677d1ffe4d
Simon <simon.provost@ymail.com>
2025-04-14 13:56:05 +0200

    Add category page controller and update route handler to use new controller

---
+++ src/api/category/controllers/categorypage.ts
@@ -0,0 +1,6 @@
+module.exports = {
+    async findCategoryPage(ctx) {
+        // Votre logique pour récupérer les données de la page catégorie
+        ctx.send('Données de la page catégorie');
+    },
+};
