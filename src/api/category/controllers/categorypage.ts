e1fe037e5c6025eb3cb78a74ee6571b7d54a136d
Simon PROVOST <sprovost@secom.fr>
2025-04-14 16:07:33 +0200

    Enhance category page API to retrieve category and related articles by slug

--- src/api/category/controllers/categorypage.ts
+++ src/api/category/controllers/categorypage.ts
@@ -1,6 +1,40 @@
-module.exports = {
+export default {
     async findCategoryPage(ctx) {
-        // Votre logique pour récupérer les données de la page catégorie
-        ctx.send('Données de la page catégorie');
+      const { slug } = ctx.params;
+  
+      // Sécurité : enlever slash final s’il y en a
+      const baseSlug = slug
+  
+      // 1. Catégories dont le slug commence par le slug donné
+    const categories = await strapi.entityService.findMany('api::category.category', {
+        filters: {
+          slug: {
+            $startsWith: baseSlug,
+          },
+        },
+        }
+    );
+  
+    //   // 2. Trouver la catégorie exacte
+      const currentCategory = categories.find(c => c.slug === baseSlug);
+
+    console.log(baseSlug,categories)
+
+      console.log("currentCategory", currentCategory);
+  
+      // 3. Articles liés uniquement à la catégorie actuelle
+      const articles = await strapi.entityService.findMany('api::product.product', {
+        filters: {
+          category: { id: currentCategory?.id },
+        },
+      });
+  
+      ctx.body = {
+        currentCategory,
+        children: categories.filter(c => c.slug !== baseSlug),
+        articles,
+      };
+
+    // ctx.send("Prout");
     },
-};
+  };
